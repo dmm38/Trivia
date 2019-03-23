c66b9de4562cf96320ed5eaad4760e892bd7829d
dmm38 <47959145+dmm38@users.noreply.github.com>
2019-03-22 20:34:39 -0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,39 @@
+    // Object for storing quiz questions
+    var questions = [
+        {
+            question : 'El paso uno de organizaci\xf3n pretende:',
+            answers : ['Conocer las vulnerabilidades de la comunidad. ', 'Establecer el Comit\xE9 Comunal y distribuir funciones.  ', 'Ubicar zonas de riesgo. ','Organizar a la familia en temas de desastres.  '],
+            correctAnswer : 1, // questionIndex of correct anwers
+            imageId : '1' // ID for triggering image change
+        },
+        {
+            question : ' Despu\xE9s de identificar las amenazas que pueden afectar mi comunidad debo:',
+            answers : ['Conocer las organizaciones existentes en la comunidad. ', 'Formar un comit\xE9.  ', 'Nombrar un coordinador.  ', 'Recolectar datos personales de vecinos.'],
+            correctAnswer : 0, 
+            imageId : '2'
+        },
+        {
+            question : 'Cu\xE1les factores de vulnerabilidad  hay que tomar en cuenta seg\xFAn el paso 3 del plan? ',
+            answers : ['Personas con limitaciones f\xEDsicas. ', 'Viviendas con problemas de acceso.  ', 'Rutas alternativas de acceso a la comunidad.  ', 'Todas las anteriores. '],
+            correctAnswer : 3, 
+            imageId : '3'
+        },
+        {
+            question : 'Cu\xE1l de las siguientes NO es una etapa del plan de acci\xf3n?  ',
+            answers : ['Acci\xf3n de prevenci\xf3n, mitigaci\xf3n y preparaci\xf3n.  ', 'Acciones de respuesta.  ', 'Acci\xf3n de registro de datos.  ', 'Acciones de rehabilitaci\xf3n y reconstrucci\xf3n. '],
+            correctAnswer : 2,
+            imageId : '4'
+        },
+        {
+            question : 'La creaci\xf3n del mapa de riesgos y recursos de la comunidad a cual paso del Plan comunal de emergencias corresponde:',
+            answers : ['El primer paso. ', 'El \xFAltimo paso.  ', 'El cuarto paso posterior a conocer las vulnerabilidades. ', ' El paso 3 posterior a conocer las amenazas.  '],
+            correctAnswer : 2, 
+            imageId : '5'
+        }
+        
+        
+        
+        
+        
+        
+    ];
